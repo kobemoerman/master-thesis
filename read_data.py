61d9ca4cd477aa16cdbc789f4d80d3a9a8f49f69
kobemoerman <kobe.moerman@outlook.com>
2023-03-24 15:42:20 +0100

    normalization of joint positions

--- read_data.py
+++ read_data.py
@@ -1,4 +1,5 @@
 import os
+import cv2
 import h5py
 import random
 import argparse
@@ -7,46 +8,58 @@ import matplotlib.pyplot as plt
 
 # (192, 112)
 
+bh, bw = (192, 112)
 h, w = (240, 320)
 data_h5 = os.getcwd() + "/data/" 
 
-def setup_depth_map(data_list):
+def setup_labels(data_list, is_valid):
+    cdata = []
+
+    for i, label in enumerate(data_list):
+        if is_valid[i] == 0: continue
+        ca = label[1]  # neck
+        cb = label[8]  # torso
+        cjnt = (ca + cb) * 0.5
+
+        pose = label - cjnt  # centered
+        pose = 2 * (pose - np.min(pose)) / (np.max(pose) - np.min(pose)) - 1  # scale
+
+        cdata.append(pose)
+
+    return np.asarray(cdata)
+
+def setup_depth_map(data_list, is_valid):
     cdata = []
 
     cw = 56
     ch = 48
-    for img in data_list:
+    for i, img in enumerate(data_list):
+        if is_valid[i] == 0: continue
         cimg = np.asarray(img[ch:, int((w/2)-cw):int((w/2)+cw)])
         norm = (cimg - np.min(cimg)) / (np.max(cimg) - np.min(cimg))
         cdata.append(norm)
     
     return np.asarray(cdata)
 
-def prepare_train_data():
-    f = h5py.File(data_h5 + 'ITOP_side_train_labels.h5', 'r')
+def prepare_model_data(type):
+    assert type == 'train' or type == 'test'
+
+    f = h5py.File(data_h5 + 'ITOP_side_' + type + '_labels.h5', 'r')
+    is_valid = np.asarray(f.get('is_valid'))
     label = np.asarray(f.get('real_world_coordinates'))
+    label = setup_labels(label, is_valid)
 
-    f = h5py.File(data_h5 + 'ITOP_side_train_depth_map.h5', 'r')
+    f = h5py.File(data_h5 + 'ITOP_side_' + type + '_depth_map.h5', 'r')
     depth_map = np.asarray(f.get('data'))
-    depth_map = setup_depth_map(depth_map)
-
-    return label, depth_map
-
-def prepare_test_data():
-    f = h5py.File(data_h5 + 'ITOP_side_test_labels.h5', 'r')
-    label = np.asarray(f.get('real_world_coordinates'))
-
-    f = h5py.File(data_h5 + 'ITOP_side_test_depth_map.h5', 'r')
-    depth_map = np.asarray(f.get('data'))
-    depth_map = setup_depth_map(depth_map)
+    depth_map = setup_depth_map(depth_map, is_valid)
 
     return label, depth_map
 
 def save_modified_data():
-    train_label, train_data = prepare_train_data()
-    test_label, test_data = prepare_test_data()
+    train_label, train_data = prepare_model_data('train')
+    test_label, test_data = prepare_model_data('test')
 
-    with h5py.File(data_h5 + 'dataset_itop.hdf5', 'w') as hf:
+    with h5py.File(data_h5 + 'norm_dataset_itop.hdf5', 'w') as hf:
         hf.create_dataset('x_train', data=train_data, shape=train_data.shape, compression='gzip', chunks=True)
         hf.create_dataset('y_train', data=train_label, shape=train_label.shape, compression='gzip', chunks=True)
         hf.create_dataset('x_test', data=test_data, shape=test_data.shape, compression='gzip', chunks=True)
@@ -68,7 +81,7 @@ def visualise_data(data):
     plt.axis('off')
     plt.show()
 
-def visualise_label(data):
+def visualise_label(data, norm=False):
     joint_id_to_name = {
         0: 'Head',        8: 'Torso',
         1: 'Neck',        9: 'R Hip',
@@ -82,7 +95,21 @@ def visualise_label(data):
 
     joint_map = [[0,1],[1,2],[1,3],[2,4],[3,5],[4,6],[5,7],[1,8],[8,9],[8,10],[9,10],[9,11],[10,12],[11,13],[12,14]]
 
-    dx, dy, dz = zip(*data)
+    if norm:
+        dx, dy, dz = zip(*data)
+
+        ca = data[1]  # neck
+        cb = data[8]  # torso
+        cjnt = (ca + cb) * 0.5
+        pose = data - cjnt  # centered
+
+        pose = 2 * (pose - np.min(pose)) / (np.max(pose) - np.min(pose)) - 1  # scale
+
+        dx, dy, dz = zip(*pose)
+        
+    else:
+        dx, dy, dz = zip(*data)
+        
 
     fig = plt.figure()
     ax = plt.axes(projection='3d')
@@ -126,11 +153,18 @@ def main():
     print(idx)
 
     f = h5py.File(data_h5 + 'ITOP_side_test_labels.h5', 'r')
-    pos, visible = f.get('real_world_coordinates'), f.get('visible_joints')    
-    if _disp: visualise_label(pos[idx])
+    pos, valid = f.get('real_world_coordinates'), f.get('is_valid')
+    
+    unique, counts = np.unique(valid, return_counts=True)
+    print(dict(zip(unique, counts)))
+
+    if _disp: 
+        visualise_label(pos[idx], False)
+        visualise_label(pos[idx], True)
 
     f = h5py.File(data_h5 + 'ITOP_side_test_depth_map.h5', 'r')
-    data, ids = f.get('data'), f.get('ids')
+    data = f.get('data')
+
     if _disp: visualise_data(data[idx])
 
     if _export: save_modified_data()
