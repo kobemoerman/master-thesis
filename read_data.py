bab69f0567780b1a58ceb428c87178ada8037fed
Kobe Moerman <kobemoerman@MacBeth-Air.local>
2023-04-07 09:40:15 +0200

    reformated reading the data

--- read_data.py
+++ read_data.py
@@ -1,4 +1,3 @@
-import os
 import cv2
 import h5py
 import random
@@ -6,197 +5,70 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
-# (192, 112)
+from utils import data_position_noise, data_segmented_noise, visualise_data, visualise_label
 
 bh, bw = (192, 112)
 h, w = (240, 320)
-data_h5 = os.getcwd() + "/data/" 
 
-def setup_labels(data_list, is_valid):
-    cdata = []
+def normalize_labels(data):
+    data = np.array(data).astype(np.float32).reshape((-1, 45))
+    data_mean = np.expand_dims(np.mean(data, axis=-1), axis=-1)
+    data_std  = np.expand_dims(np.std(data, axis=-1), axis=-1)
 
-    for i, label in enumerate(data_list):
-        if is_valid[i] == 0: continue
-        ca = label[1]  # neck
-        cb = label[8]  # torso
-        cjnt = (ca + cb) * 0.5
+    return (data - data_mean) / data_std
 
-        pose = label - cjnt  # centered
-        pose = 2 * (pose - np.min(pose)) / (np.max(pose) - np.min(pose)) - 1  # scale
+def normalize_data(data):
+    return cv2.normalize(np.float32(data), None, 0, 1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
 
-        cdata.append(pose)
-
-    return np.asarray(cdata)
-
-def setup_depth_map(data_list, cntr, is_valid):
-    cdata = []
-
+def crop_data(data, id):
     cw = 56
     ch = 48
-    for i, img in enumerate(data_list):
-        if is_valid[i] == 0: continue
-        nimg = data_segmented_noise(img, cntr)
-        cimg = np.asarray(nimg[ch:, int((w/2)-cw):int((w/2)+cw)])
-        norm = (cimg - np.min(cimg)) / (np.max(cimg) - np.min(cimg))
-        cdata.append(norm)
-    
-    return np.asarray(cdata)
 
-def data_position_noise(data, joints):
-    fig = plt.figure()
+    cdict = {}
+    for key, img in zip(id, data):
+        cimg = np.asarray(img[ch:, int((w/2)-cw):int((w/2)+cw)])
+        cdict[key] = cv2.resize(np.float32(cimg), (56, 96))
 
-    ax1 = fig.add_subplot(1,3,1)
-    ax1.imshow(data)
-
-    ax2 = fig.add_subplot(1,3,2)
-    ax2.imshow(data)
-
-    print(joints)
-
-    for j in joints:
-        ax2.scatter(x=j[0], y=j[1], c='r', s=40)
-
-    ax3 = fig.add_subplot(1,3,3)
-    cdata = np.copy(data)
-    for j in joints:
-        for dx in range(j[0]-10, j[0]+10):
-            for dy in range(j[1]-10, j[1]+10):
-                if dx < 0 or dy < 0 or dx >= w or dy >= h: continue
-                data[dy][dx] = -1
-
-    row, col = np.where(data == -1)
-    noise_fact = 0.4
-
-    noise_img = cdata + noise_fact * np.random.normal(loc=0.0, scale=1.0, size=data.shape)
-    cdata[row,col] = noise_img[row,col]
-
-    ax3.imshow(cdata)
-
-    plt.show()
-
-def data_segmented_noise(data, cntr):
-    cw = 56
-    ch = 48
-    data = np.asarray(data[ch:, int((w/2)-cw):int((w/2)+cw)])
-    cntr = np.asarray(cntr[ch:, int((w/2)-cw):int((w/2)+cw)])
-    
-    fig = plt.figure()
-    ax1 = fig.add_subplot(1,4,1)
-
-    ax1.imshow(data)
-
-    row, col = np.where(data < 3.4)
-    
-    data = (data - np.min(data)) / (np.max(data) - np.min(data))
-    cdata = np.copy(data)
-    cdata[row, col] = -1
-    
-    ax2 = fig.add_subplot(1,4,2)
-    ax2.imshow(cdata)
-    ax3 = fig.add_subplot(1,4,3)
-    ax3.imshow(cntr)
-
-    noise_fact = 0.4
-    noise_img  = data + noise_fact * np.random.normal(loc=0.0, scale=1.0, size=data.shape)
-    cdata[row,col] = noise_img[row,col]
-
-    cdata = np.clip(cdata, 0., 1.)
-
-    ax4 = fig.add_subplot(1,4,4)
-    ax4.imshow(cdata)
-
-    plt.show()
+    return cdict
 
 
 def prepare_model_data(type):
     assert type == 'train' or type == 'test'
 
-    f = h5py.File(data_h5 + 'ITOP_side_' + type + '_labels.h5', 'r')
-    is_valid = np.asarray(f.get('is_valid'))
-    label = np.asarray(f.get('real_world_coordinates'))
-    label = setup_labels(label, is_valid)
+    l = h5py.File('./data/ITOP_side_' + type + '_labels.h5', 'r')
+    d = h5py.File('./data/ITOP_side_' + type + '_depth_map.h5', 'r')
 
-    f = h5py.File(data_h5 + 'ITOP_side_' + type + '_depth_map.h5', 'r')
-    depth_map = np.asarray(f.get('data'))
-    cntr = np.asarray(f.get('segmentation'))
-    depth_map = setup_depth_map(depth_map, cntr, is_valid)
+    l_id, pos, valid = np.asarray(l.get('id')), np.asarray(l.get('real_world_coordinates')), np.asarray(l.get('is_valid'))
+    d_id, data = np.asarray(d.get('id')), np.asarray(d.get('data'))
 
-    return label, depth_map
+    is_valid = np.where(valid == 1)
+
+    l_id, pos = l_id[is_valid], pos[is_valid]
+    d_id, data = d_id[is_valid], data[is_valid]
+
+    norm_pos = normalize_labels(pos)
+    labels = dict(zip(l_id, norm_pos))
+
+    data = normalize_data(data)
+    images = crop_data(data, d_id)
+
+    x, y = [], []
+    for key in images:
+        x.append(images[key])
+        y.append(labels[key])
+
+    return np.asarray(y, dtype=np.float32), np.asarray(x, dtype=np.float32)
 
 def save_modified_data():
     train_label, train_data = prepare_model_data('train')
     test_label, test_data = prepare_model_data('test')
 
-    with h5py.File(data_h5 + 'norm_dataset_itop.hdf5', 'w') as hf:
+    with h5py.File('./data/norm_dataset_itop.hdf5', 'w') as hf:
         hf.create_dataset('x_train', data=train_data, shape=train_data.shape, compression='gzip', chunks=True)
         hf.create_dataset('y_train', data=train_label, shape=train_label.shape, compression='gzip', chunks=True)
         hf.create_dataset('x_test', data=test_data, shape=test_data.shape, compression='gzip', chunks=True)
         hf.create_dataset('y_test', data=test_label, shape=test_label.shape, compression='gzip', chunks=True)
     
-
-def visualise_data(data):
-    cw = 56
-    ch = 48
-
-    data_reshape = data[ch:, int((w/2)-cw):int((w/2)+cw)]
-
-    print(data_reshape.shape)
-
-    plt.matshow(data_reshape, cmap=plt.cm.viridis, interpolation='bicubic')
-    plt.colorbar()
-    
-    plt.grid(visible=None)
-    plt.axis('off')
-    plt.show()
-
-def visualise_label(data, norm=False):
-    joint_id_to_name = {
-        0: 'Head',        8: 'Torso',
-        1: 'Neck',        9: 'R Hip',
-        2: 'R Shoulder',  10: 'L Hip',
-        3: 'L Shoulder',  11: 'R Knee',
-        4: 'R Elbow',     12: 'L Knee',
-        5: 'L Elbow',     13: 'R Foot',
-        6: 'R Hand',      14: 'L Foot',
-        7: 'L Hand',
-    }
-
-    joint_map = [[0,1],[1,2],[1,3],[2,4],[3,5],[4,6],[5,7],[1,8],[8,9],[8,10],[9,10],[9,11],[10,12],[11,13],[12,14]]
-
-    if norm:
-        dx, dy, dz = zip(*data)
-
-        ca = data[1]  # neck
-        cb = data[8]  # torso
-        cjnt = (ca + cb) * 0.5
-        pose = data - cjnt  # centered
-
-        pose = 2 * (pose - np.min(pose)) / (np.max(pose) - np.min(pose)) - 1  # scale
-
-        dx, dy, dz = zip(*pose)
-        
-    else:
-        dx, dy, dz = zip(*data)
-        
-
-    fig = plt.figure()
-    ax = plt.axes(projection='3d')
-
-    for j in range(len(joint_id_to_name)):
-        ax.text(dx[j]+.025, dy[j]+.025, dz[j], '%s' % str(joint_id_to_name.get(j)), size=7)
-
-    for parent, child in joint_map:
-        ax.plot([dx[parent], dx[child]], 
-                [dy[parent], dy[child]], 
-                [dz[parent], dz[child]], 'k-')
-
-    p = ax.scatter3D(dx, dy, dz, c=dz)
-    fig.colorbar(p)
-
-    ax.view_init(elev=90, azim=-90)
-    plt.grid(visible=None)
-    plt.axis('off')
-    plt.show()
 
 def main():
     """
@@ -216,31 +88,32 @@ def main():
     if args.export not in ("True", "False"): raise ValueError("-export must be \"True\" or \"False\".")
     else: _export = args.export == "True"
 
-    # 500 test is bad
-    idx = random.randint(0, 10000)
-    print(idx)
+    f = h5py.File('./data/ITOP_side_test_labels.h5', 'r')
+    id, pos, valid = np.asarray(f.get('id')), np.asarray(f.get('real_world_coordinates')), np.asarray(f.get('is_valid'))
 
-    f = h5py.File(data_h5 + 'ITOP_side_test_labels.h5', 'r')
-    pos, valid = f.get('real_world_coordinates'), f.get('is_valid')
-    
     unique, counts = np.unique(valid, return_counts=True)
     print(dict(zip(unique, counts)))
 
-    if _disp: 
-        visualise_label(pos[idx], False)
-        visualise_label(pos[idx], True)
+    is_valid = np.where(valid == 1)
+    norm_pos = normalize_labels(pos[is_valid])
+    labels = dict(zip(id[is_valid], norm_pos))
 
-    cntr = f.get('segmentation')
-    img_pos = f.get('image_coordinates')
-    f = h5py.File(data_h5 + 'ITOP_side_test_depth_map.h5', 'r')
-    data = f.get('data')
+    idx = random.randint(0, len(labels))
+    if _disp: visualise_label(norm_pos[idx])
 
-    data_position_noise(data[idx], img_pos[idx])
-    data_segmented_noise(data[idx], cntr[idx])
+    f = h5py.File('./data/ITOP_side_test_depth_map.h5', 'r')
+    id, data = np.asarray(f.get('id')), np.asarray(f.get('data'))
+
+    data = normalize_data(data[is_valid])
+    dimg = crop_data(data, id[is_valid])
 
     if _disp: visualise_data(data[idx])
-
     if _export: save_modified_data()
+
+    # cntr = f.get('segmentation')
+    # img_pos = f.get('image_coordinates')
+    # data_position_noise(data[idx], img_pos[idx])
+    # data_segmented_noise(data[idx], cntr[idx])
 
 
 
--- utils.py
+++ utils.py
@@ -1,35 +1,119 @@
+import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-def calculate_ap(precision, recall):
-    """Calculate Average Precision (AP) given precision and recall arrays."""
-    # Append 0 and 1 to precision and recall arrays to simplify calculations
-    precision = np.concatenate(([0], precision, [1]))
-    recall = np.concatenate(([0], recall, [0]))
-    # Calculate the area under the precision-recall curve
-    ap = np.sum(np.diff(recall) * precision[:-1])
-    return ap
+h, w = (240, 320)
 
-def calculate_map(joint_pred, joint_gt, threshold=10):
-    """Calculate Mean Average Precision (MAP) at a given threshold for each joint label."""
-    num_samples, num_joints = joint_pred.shape[:2]
-    ap_scores = np.zeros(num_joints)
-    for i in range(num_joints):
-        # Extract x, y and z coordinates of predicted and ground truth joint locations
-        pred_x, pred_y, pred_z = joint_pred[:, i, 0], joint_pred[:, i, 1], joint_pred[:, i, 2]
-        gt_x, gt_y, gt_z = joint_gt[:, i, 0], joint_gt[:, i, 1], joint_gt[:, i, 2]
-        # Calculate Euclidean distance between predicted and ground truth joint locations
-        distances = np.sqrt((pred_x - gt_x)**2 + (pred_y - gt_y)**2 + (pred_z - gt_z)**2)
-        # Convert the threshold from cm to m
-        threshold_m = threshold / 100
-        # Calculate precision and recall at the given threshold
-        true_positives = distances <= threshold_m
-        false_positives = distances > threshold_m
-        true_positives_cumulative = np.cumsum(true_positives)
-        false_positives_cumulative = np.cumsum(false_positives)
-        precision = true_positives_cumulative / (true_positives_cumulative + false_positives_cumulative)
-        recall = true_positives_cumulative / np.sum(true_positives)
-        # Calculate Average Precision (AP) for the current joint label
-        ap_scores[i] = calculate_ap(precision, recall)
-    # Calculate Mean Average Precision (MAP) across all joint labels
-    map_score = np.mean(ap_scores)
-    return map_score, ap_scores
+def visualise_data(data):
+    
+    cw = 56
+    ch = 48
+    data = np.asarray(data[ch:, int((w/2)-cw):int((w/2)+cw)], dtype=np.float32)
+    data = cv2.resize(data, (56, 96))
+
+    print(data.shape)
+    plt.matshow(data, cmap=plt.cm.viridis, interpolation='bicubic')
+    plt.colorbar()
+    
+    plt.grid(visible=None)
+    plt.axis('off')
+    plt.show()
+
+def visualise_label(data):
+    joint_id_to_name = {
+        0: 'Head',        8: 'Torso',
+        1: 'Neck',        9: 'R Hip',
+        2: 'R Shoulder',  10: 'L Hip',
+        3: 'L Shoulder',  11: 'R Knee',
+        4: 'R Elbow',     12: 'L Knee',
+        5: 'L Elbow',     13: 'R Foot',
+        6: 'R Hand',      14: 'L Foot',
+        7: 'L Hand',
+    }
+
+    joint_map = [[0,1],[1,2],[1,3],[2,4],[3,5],[4,6],[5,7],[1,8],[8,9],[8,10],[9,10],[9,11],[10,12],[11,13],[12,14]]
+
+    data = np.reshape(data, (15, 3))
+    dx, dy, dz = zip(*data)  
+
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+
+    for j in range(len(joint_id_to_name)):
+        ax.text(dx[j]+.025, dy[j]+.025, dz[j], '%s' % str(joint_id_to_name.get(j)), size=7)
+
+    for parent, child in joint_map:
+        ax.plot([dx[parent], dx[child]], 
+                [dy[parent], dy[child]], 
+                [dz[parent], dz[child]], 'k-')
+
+    p = ax.scatter3D(dx, dy, dz, c=dz)
+    fig.colorbar(p)
+
+    ax.view_init(elev=90, azim=-90)
+    plt.grid(visible=None)
+    plt.axis('off')
+    plt.show()
+
+def data_position_noise(data, joints):
+    fig = plt.figure()
+
+    ax1 = fig.add_subplot(1,3,1)
+    ax1.imshow(data)
+
+    ax2 = fig.add_subplot(1,3,2)
+    ax2.imshow(data)
+
+    for j in joints:
+        ax2.scatter(x=j[0], y=j[1], c='r', s=40)
+
+    ax3 = fig.add_subplot(1,3,3)
+    cdata = np.copy(data)
+    for j in joints:
+        for dx in range(j[0]-10, j[0]+10):
+            for dy in range(j[1]-10, j[1]+10):
+                if dx < 0 or dy < 0 or dx >= w or dy >= h: continue
+                data[dy][dx] = -1
+
+    row, col = np.where(data == -1)
+    noise_fact = 0.4
+
+    noise_img = cdata + noise_fact * np.random.normal(loc=0.0, scale=1.0, size=data.shape)
+    cdata[row,col] = noise_img[row,col]
+
+    ax3.imshow(cdata)
+
+    plt.show()
+
+def data_segmented_noise(data, cntr):
+    cw = 56
+    ch = 48
+    data = np.asarray(data[ch:, int((w/2)-cw):int((w/2)+cw)])
+    cntr = np.asarray(cntr[ch:, int((w/2)-cw):int((w/2)+cw)])
+    
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1,4,1)
+
+    ax1.imshow(data)
+
+    row, col = np.where(data < 3.4)
+    
+    data = (data - np.min(data)) / (np.max(data) - np.min(data))
+    cdata = np.copy(data)
+    cdata[row, col] = -1
+    
+    ax2 = fig.add_subplot(1,4,2)
+    ax2.imshow(cdata)
+    ax3 = fig.add_subplot(1,4,3)
+    ax3.imshow(cntr)
+
+    noise_fact = 0.4
+    noise_img  = data + noise_fact * np.random.normal(loc=0.0, scale=1.0, size=data.shape)
+    cdata[row,col] = noise_img[row,col]
+
+    cdata = np.clip(cdata, 0., 1.)
+
+    ax4 = fig.add_subplot(1,4,4)
+    ax4.imshow(cdata)
+
+    plt.show()
